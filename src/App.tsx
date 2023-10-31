3120776bb2e2b36c5ec59c2eb2df16184fee3025
estebanCastaneira <estebanc_b@hotmail.com>
2023-10-31 13:53:33 -0300

    font configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import "./styles/fonts.css";
 function App() {
-  return <></>;
+  return (
+    <>
+      <main className="my-10  mx-auto max-w-4xl">
+        <h1 className="m-5 font-light">Match ReportGg</h1>
+        {/* <Navbar /> */}
+        <div className="mx-2">
+          <Routes>
+            <Route path="/" />
+            <Route path="/lineup" />
+            <Route path="/zones" />
+          </Routes>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
